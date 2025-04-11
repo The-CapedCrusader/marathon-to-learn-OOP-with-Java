a81941d658cc6d68560be9967a1d49fea9be6bc3
The-CapedCrusader <6209tanvir@gmail.com>
2025-04-11 19:37:43 +0600

    Array

---
+++ src/main/java/com/example/javamarathon/Array.java
@@ -0,0 +1,12 @@
+package com.example.javamarathon;
+import java.util.*;
+public class Array {
+    public static void main (String[] args){
+        String[] cars = {"Toyota", "Tesla"};
+        System.out.println(Arrays.toString(cars));
+
+        for (String var : cars){
+            System.out.println(var);
+        }
+    }
+}
