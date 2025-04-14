d9133645aae2ac44b5e474caf367cc9e1bf5b39e
The-CapedCrusader <6209tanvir@gmail.com>
2025-04-15 00:36:39 +0600

    Classes

---
+++ src/main/java/com/example/javamarathon/Practice.java
@@ -0,0 +1,16 @@
+package com.example.javamarathon;
+
+public class Practice {
+
+    int x = 10;
+    int y = 20;
+
+    public static void main(String[] args){
+        Practice p = new Practice();
+
+        p.PrintValue();
+    }
+    public void PrintValue(){
+        System.out.println(x);
+    }
+}
