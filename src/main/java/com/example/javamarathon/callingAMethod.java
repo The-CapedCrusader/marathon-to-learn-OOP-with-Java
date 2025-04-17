456ded7509ad492b9bd65eb9a62e919e4a1b30b5
The-CapedCrusader <6209tanvir@gmail.com>
2025-04-17 09:56:16 +0600

    Callin a Method

---
+++ src/main/java/com/example/javamarathon/callingAMethod.java
@@ -0,0 +1,15 @@
+package com.example.javamarathon;
+
+public class callingAMethod {
+    public static void main(String[] args){
+        double s = sum(9,8);
+
+        System.out.println(s);
+
+
+
+    }
+    public static double sum(int a, int b){
+        return a+b;
+    }
+}
